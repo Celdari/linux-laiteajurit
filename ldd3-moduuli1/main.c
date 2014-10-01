a87cbc5008432ad0b1310752f6d552ef8751472c
Celdari <ari.suoyrjo@gmail.com>
2014-10-01 08:36:07 +0300

    ldd3-moduuli1 start

---
+++ ldd3-moduuli1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: t4suar00
+ *
+ * Created on 1. lokakuuta 2014, 8:35
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
