5a9734d41f3318992bffe1276b5fc9599d533a74
Celdari <ari.suoyrjo@gmail.com>
2014-10-02 14:05:36 +0300

    editin x or y in /sys/modules/hello/parameters now is shown in dmesg

--- ldd3-read-only/misc-modules/hello.c
+++ ldd3-read-only/misc-modules/hello.c
@@ -3,21 +3,54 @@
  */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+//#include <fcntl.h>
+//#include <sys/stat.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
 //Module parameters
-static int my_parameter = 0;
-module_param(my_parameter, int, S_IRUGO | S_IWUSR);
+static int x, y, tulo, lukujen_maara = 0;
+
+static int tulo_get(char *buffer, const struct kernel_param *kp) {
+    return scnprintf(buffer, PAGE_SIZE, "%d", *((int *)kp->arg));
+}
+
+static int tulo_set(const char *val, const struct kernel_param *kp) {
+  long l;
+  int ret;
+  
+  ret = kstrtoul(val, 0, &l);
+  if (ret < 0 || ((int)l != l))	{
+      printk(KERN_ERR "Failed\n");
+      return ret < 0 ? ret : -EINVAL;
+  }
+    
+  *((int *)kp->arg) = l;
+
+  printk(KERN_ALERT "Hello: New param %ld\n", l);
+
+  return 0;
+}
+
+static struct kernel_param_ops tulo_ops = {
+    .set = tulo_set,
+    .get = tulo_get
+};
+
+module_param_cb(x, &tulo_ops, &tulo, S_IRUGO | S_IWUSR);
+module_param_cb(y, &tulo_ops, &tulo, S_IRUGO | S_IWUSR);
+module_param(lukujen_maara, int, S_IRUGO);
+module_param_cb(tulo, &tulo_ops, &tulo, S_IRUGO | S_IWUSR);
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world! Tulo: %d\n", tulo_ops);
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world. %d\n", my_parameter);
+	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
